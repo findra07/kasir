70ef6407f0e35c7a6ccdcd32ad8bbcb938a90b03
findra07 <anipfindra@gmail.com>
2021-10-07 16:04:16 +0700

    first commit

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+
+	public function index()
+	{
+
+		if ($this->session->userdata('status') == 'login') {
+			$this->load->view('dashboard');
+		} else {
+			$this->load->view('login');
+		}
+	}
+}
